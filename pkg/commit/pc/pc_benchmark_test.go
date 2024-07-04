b33b1fb89c4bf734bc741d53927fa77aad59adb3
JMaKro <maximilian.kroschewski@gmail.com>
2024-07-04 15:33:55 +0200

    add pc benchmarks

---
+++ pkg/commit/pc/pc_benchmark_test.go
@@ -0,0 +1,39 @@
+package pc
+
+import (
+	"testing"
+	"time"
+)
+
+func BenchmarkPCCommit(b *testing.B) {
+	pc := Setup(nil)
+	msg := []byte("test message")
+
+	b.ResetTimer()
+	// Run the benchmark
+	start := time.Now()
+	for i := 0; i < b.N; i++ {
+		pc.Commit(msg)
+	}
+	elapsed := time.Since(start)
+	b.ReportMetric(float64(elapsed.Milliseconds())/float64(b.N), "ms/op")
+}
+
+func BenchmarkPCOpen(b *testing.B) {
+	pc := Setup(nil)
+	msg := []byte("test message")
+
+	c, o := pc.Commit(msg)
+
+	b.ResetTimer()
+	// Run the benchmark
+	start := time.Now()
+	for i := 0; i < b.N; i++ {
+		isValid := pc.Open(msg, &c, &o)
+		if !isValid {
+			b.Fatal("open failed")
+		}
+	}
+	elapsed := time.Since(start)
+	b.ReportMetric(float64(elapsed.Milliseconds())/float64(b.N), "ms/op")
+}
