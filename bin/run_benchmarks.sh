1c603bb31507d3fa9e1a447571f9b1e4c9bdd915
JMaKro <maximilian.kroschewski@gmail.com>
2024-12-09 17:11:45 +0100

    typos

--- bin/run_benchmarks.sh
+++ bin/run_benchmarks.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo "Running benchmarks..."
+echo "Running benchmarks ..."
 
 # Other parameters: -count=5 -benchtime=10s
 # Remark: The timeout has been set to 120 minutes as benchmarking the hash-based proofs (via a SNARK) takes a significant amount of time
 go test -bench=. -timeout=120m -benchmem ./benchmark > benchmark_results.txt
 
-echo "Benchmarks completed. Results saved to benchmark_results.txt."
+echo "Benchmarks completed. Results saved to benchmark_results.txt"
