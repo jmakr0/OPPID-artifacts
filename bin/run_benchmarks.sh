f8c7fbb3a15a1f4fdc81e53fe9e6acbd7a8da1f5
JMaKro <maximilian.kroschewski@gmail.com>
2024-12-09 22:54:52 +0100

    save results to arg file

--- bin/run_benchmarks.sh
+++ bin/run_benchmarks.sh
@@ -2,8 +2,11 @@
 
 echo "Running benchmarks ..."
 
+# Check if a command line argument is passed
+RESULTS_FILE=${RESULTS_FILE:-benchmark_results.log}
+
 # Other parameters: -count=5 -benchtime=10s
 # Remark: The timeout has been set to 120 minutes as benchmarking the hash-based proofs (via a SNARK) takes a significant amount of time
-go test -bench=. -timeout=120m -benchmem ./benchmark > benchmark_results.txt
+go test -bench=. -timeout=120m -benchmem ./benchmark > "$RESULTS_FILE"
 
-echo "Benchmarks completed. Results saved to benchmark_results.txt"
+echo "Benchmarks completed. Results saved to $RESULTS_FILE"
