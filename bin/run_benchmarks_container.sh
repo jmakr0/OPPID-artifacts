64e881adb2ef9d3451c700d442f4d5c446d27c3a
JMaKro <maximilian.kroschewski@gmail.com>
2024-12-09 22:55:37 +0100

    mount results back to host

--- bin/run_benchmarks_container.sh
+++ bin/run_benchmarks_container.sh
@@ -4,10 +4,10 @@ RESULTS_FILE=${RESULTS_FILE:-benchmark_results.log}
 
 echo "Build container ..."
 
-docker build -t pets25-oppid .
+docker build -t pets25-oppid-img .
 
 echo "Execute container ..."
-docker run --name oppid-bench --env RESULTS_FILE="/oppid/export/$RESULTS_FILE" --volume "$(pwd):/oppid/export" pets25-oppid
+docker run --name oppid-bench --env RESULTS_FILE="/oppid/export/$RESULTS_FILE" --volume "$(pwd):/oppid/export" pets25-oppid-img
 docker rm oppid-bench > /dev/null 2>&1
 
-echo "Container terminated, results saved to $RESULTS_FILE"
+echo "Container terminated, results locally saved to $RESULTS_FILE"
