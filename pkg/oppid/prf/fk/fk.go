d61f0c8610a08662c006fb4e444313d48baaef78
JMaKro <maximilian.kroschewski@gmail.com>
2024-12-10 14:21:10 +0100

    oppid / other package separation

---
+++ pkg/oppid/prf/fk/fk.go
@@ -0,0 +1,22 @@
+package fk
+
+import (
+	DLPRF "OPPID/pkg/oppid/prf/dl"
+	HMACPRF "OPPID/pkg/oppid/prf/hmac256"
+	"OPPID/pkg/oppid/utils"
+	GG "github.com/cloudflare/circl/ecc/bls12381"
+)
+
+const dstStr = "OPPID_BLS12384_XMD:SHA-256_FK_"
+
+type Key = HMACPRF.Key
+
+func KeyGen() *Key {
+	return HMACPRF.KeyGen()
+}
+
+func Eval(k *Key, msg1, msg2 []byte) *GG.G1 {
+	y := HMACPRF.Eval(k, msg2)
+	key := utils.HashToScalar(y, []byte(dstStr))
+	return DLPRF.Eval(&key, msg1)
+}
